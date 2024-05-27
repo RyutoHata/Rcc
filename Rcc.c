d47d63de1bb19fe55e0ab91f52921cafafcd99c3
Ryuto Hata <ryuto.aiit@gmail.com>
2024-05-28 07:05:08 +0900

    Support multiplication, division, and ()

--- Rcc.c
+++ Rcc.c
@@ -24,6 +24,28 @@ char *user_input;
 
 Token *token;
 
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // integer
+} NodeKind;
+
+typedef struct Node Node;
+
+// Abstruct Syntax Tree
+struct Node {
+	NodeKind kind;
+	Node *lhs; // left-hand side
+	Node *rhs; // right-hand side
+	int val;   // only use if kind is ND_NUM
+};
+
+Node *expr();
+Node *mul();
+Node *primary();
+
 void error(char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
@@ -99,7 +121,7 @@ Token *tokenize() {
 			continue;
 		}
 
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -118,6 +140,96 @@ Token *tokenize() {
 	return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+
+	return node;
+}
+
+Node *new_node_num(int val) {
+	
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+
+	return node;
+}
+
+Node *primary() {
+	
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+Node *mul() {
+	
+	Node *node = primary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+
+Node *expr() {
+	
+	Node *node = mul();
+
+	for(;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+void gen(Node *node) {
+
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		error("Incorrect number of arguments");
@@ -126,24 +238,15 @@ int main(int argc, char **argv) {
 
 	user_input = argv[1];
 	token = tokenize();
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
 
-	printf("  mov rax, %d\n", expect_number());
+	gen(node);
 
-	while (!at_eof()) {
-
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
+	printf("  pop rax\n");
 	printf("  ret\n");	
 	return 0;
 }
